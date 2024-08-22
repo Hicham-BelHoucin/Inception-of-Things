d38440ba942c7beffbdd4ee1cd5e3aed8f687c87
Soufiane El marsi <sel-mars@e2r3p10.1337.ma>
2024-08-22 11:43:46 +0100

    autoinstall ubuntu vm wip

--- p3/scripts/_init-vm.sh
+++ p3/scripts/_init-vm.sh
@@ -3,7 +3,7 @@
 # Variables
 # UBUNTU_VERSION="20.04"
 ISO_PATH="/Users/sel-mars/goinfre/ubuntu-20.04.6-live-server-amd64.iso"
-VM_NAME="Inception-Of-Things_P3"
+VM_NAME="Inception-Of-Things-P3"
 SHARED_FOLDER="/Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/shared"
 VDI_PATH="/Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/${VM_NAME}.vdi"
 VDI_SIZE=10240
@@ -21,78 +21,40 @@ fi
 VBoxManage createvm --name "${VM_NAME}" --ostype "Ubuntu_64" --register
 VBoxManage modifyvm "${VM_NAME}" --memory ${MEMORY_SIZE} --cpus ${CPU_COUNT} --vram ${VRAM_SIZE}
 
-# Create and attach virtual hard disk
+# Create and attach virtual hard disk and IDE Controller
 VBoxManage createhd --filename "${VDI_PATH}" --size ${VDI_SIZE} --format VDI
 VBoxManage storagectl "${VM_NAME}" --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach "${VM_NAME}" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${VDI_PATH}"
+VBoxManage storagectl "${VM_NAME}" --name "IDE Controller" --add ide
+VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "${ISO_PATH}"
 
 # Set up shared folder
 VBoxManage sharedfolder add "${VM_NAME}" --name "shared" --hostpath "${SHARED_FOLDER}" --automount
 
-# Create and configure Host-Only Network
-# VBoxManage hostonlyif create
-# VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 --netmask 255.255.255.0
-# VBoxManage modifyvm "${VM_NAME}" --nic1 hostonly --hostonlyadapter1 vboxnet0
+# Disable Floppy from booting and enable Hard Disk + CD/DVD
+VBoxManage modifyvm "${VM_NAME}" --boot1 disk --boot2 dvd --boot3 none --boot4 none
 
 # Set up network port forwarding for SSH, HTTP, and HTTPS
 VBoxManage modifyvm "${VM_NAME}" --natpf1 "guestssh,tcp,,2222,,22"
 VBoxManage modifyvm "${VM_NAME}" --natpf1 "guesthttp,tcp,,80,,80"
 VBoxManage modifyvm "${VM_NAME}" --natpf1 "guesthttps,tcp,,443,,443"
 
-# # Listen for a username and password
-# # read -p "Enter a username: " USERNAME
-# read -s -p "Enter a password: " PASSWORD
-# HASHED_PASSWORD=$(echo -n "$PASSWORD" | openssl dgst -sha512)
+# Listen for a username and password
+read -p "Enter a Full Name: " FULL_NAME
+read -p "Enter a username: " USERNAME
+read -s -p "Enter a password: " PASSWORD
 
-# # Copy cloud-init configuration file replacing "password" with the hashed password and
-# # copy it to a temporary directory along with meta-data from ./confs
+# VBoxManage: error: Incomplete hostname 'Inception-Of-Things-P3' - must include both a name and a domain
+VBoxManage unattended install "${VM_NAME}" \
+    --full-user-name="${FULL_NAME}" \
+    --user="${USERNAME}" \
+    --password="${PASSWORD}" \
+    --locale=en_US \
+    --country=US \
+    --time-zone=UTC+1 \
+    --hostname="${VM_NAME}.local" \
+    --iso="${ISO_PATH}" \
+    --install-additions
 
-if [ -d /tmp/cloud-init ]; then
-    rm -rf /tmp/cloud-init
-fi
-mkdir /tmp/cloud-init
-# cp ./confs/user-data /tmp/cloud-init/user-data
-# cp ./confs/meta-data /tmp/cloud-init/meta-data
-# sed -i '' "s|passwordPlaceholder|$HASHED_PASSWORD|g" /tmp/cloud-init/user-data
-
-# !tmp
-cp ./confs/user-data /tmp/cloud-init/user-data
-cp ./confs/meta-data /tmp/cloud-init/meta-data
-# END !tmp
-
-# Create FAT-32 file and convert it to vmdk
-if [ -f /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg ]; then
-    hdiutil detach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-    rm -rf /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-fi
-hdiutil create -ov -size 100m -fs fat32 -volname cidata /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-DISK_PATH=$(hdiutil attach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg | grep FDisk_partition_scheme | awk '{print $1}')
-diskutil unmountDisk "$DISK_PATH"
-VBoxManage createmedium disk --filename /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.vmdk --format VMDK --variant RawDisk --property RawDrive="${DISK_PATH}"
-diskutil mountDisk "$DISK_PATH"
-
-cp /tmp/cloud-init/user-data /Volumes/cidata/user-data
-cp /tmp/cloud-init/meta-data /Volumes/cidata/meta-data
-
-# Make a FAT-32 formatted disk image and mount it
-# hdiutil create -ov -size 100m -fs fat32 -volname cidata /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-# hdiutil attach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-
-# Create raw disk image
-# VBoxManage internalcommands createrawvmdk -filename /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.vmdk -rawdisk /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
-
-# Attach the Ubuntu to device 0 and cloud-init ISO to device 1
-# VBoxManage storagectl "${VM_NAME}" --name "IDE Controller" --add ide
-VBoxManage storageattach "${VM_NAME}" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "${ISO_PATH}"
-VBoxManage storageattach "${VM_NAME}" --storagectl "SATA Controller" --port 2 --device 0 --type hdd --medium /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.vmdk
-
-# Enable EFI
-VBoxManage modifyvm "${VM_NAME}" --firmware efi
-
-# Disable Floppy from booting and enable Hard Disk + CD/DVD
-VBoxManage modifyvm "${VM_NAME}" --boot1 disk --boot2 dvd --boot3 none --boot4 none
-
-# Enable serial port
-VBoxManage modifyvm "${VM_NAME}" --uart1 0x3F8 4 --uartmode1 server /tmp/serial-port
 # Start the VM
 VBoxManage startvm "${VM_NAME}" --type gui
--- p3/scripts/init-vm.sh
+++ p3/scripts/init-vm.sh
@@ -7,8 +7,8 @@ VM_NAME="Inception-Of-Things-P3"
 SHARED_FOLDER="/Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/shared"
 VDI_PATH="/Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/${VM_NAME}.vdi"
 VDI_SIZE=10240
-MEMORY_SIZE=2048
-CPU_COUNT=1
+MEMORY_SIZE=4096
+CPU_COUNT=2
 VRAM_SIZE=16
 
 # Check if VM already exists
@@ -21,40 +21,76 @@ fi
 VBoxManage createvm --name "${VM_NAME}" --ostype "Ubuntu_64" --register
 VBoxManage modifyvm "${VM_NAME}" --memory ${MEMORY_SIZE} --cpus ${CPU_COUNT} --vram ${VRAM_SIZE}
 
-# Create and attach virtual hard disk and IDE Controller
+# Create and attach virtual hard disk
 VBoxManage createhd --filename "${VDI_PATH}" --size ${VDI_SIZE} --format VDI
-VBoxManage storagectl "${VM_NAME}" --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storagectl "${VM_NAME}" --name "SATA Controller" --add sata --controller IntelAhci --portcount 1
 VBoxManage storageattach "${VM_NAME}" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${VDI_PATH}"
-VBoxManage storagectl "${VM_NAME}" --name "IDE Controller" --add ide
-VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "${ISO_PATH}"
 
 # Set up shared folder
 VBoxManage sharedfolder add "${VM_NAME}" --name "shared" --hostpath "${SHARED_FOLDER}" --automount
 
-# Disable Floppy from booting and enable Hard Disk + CD/DVD
-VBoxManage modifyvm "${VM_NAME}" --boot1 disk --boot2 dvd --boot3 none --boot4 none
+# Create and configure Host-Only Network
+# VBoxManage hostonlyif create
+# VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 --netmask 255.255.255.0
+# VBoxManage modifyvm "${VM_NAME}" --nic1 hostonly --hostonlyadapter1 vboxnet0
 
 # Set up network port forwarding for SSH, HTTP, and HTTPS
-VBoxManage modifyvm "${VM_NAME}" --natpf1 "guestssh,tcp,,2222,,22"
-VBoxManage modifyvm "${VM_NAME}" --natpf1 "guesthttp,tcp,,80,,80"
-VBoxManage modifyvm "${VM_NAME}" --natpf1 "guesthttps,tcp,,443,,443"
+VBoxManage modifyvm "${VM_NAME}" --natpf1 "guest_ssh,tcp,,2222,,22"
+VBoxManage modifyvm "${VM_NAME}" --natpf1 "guest_http,tcp,,80,,80"
+VBoxManage modifyvm "${VM_NAME}" --natpf1 "guest_https,tcp,,443,,443"
 
-# Listen for a username and password
-read -p "Enter a Full Name: " FULL_NAME
-read -p "Enter a username: " USERNAME
-read -s -p "Enter a password: " PASSWORD
+# # Listen for a username and password
+# # read -p "Enter a username: " USERNAME
+# read -s -p "Enter a password: " PASSWORD
+# HASHED_PASSWORD=$(echo -n "$PASSWORD" | openssl dgst -sha512)
 
-# VBoxManage: error: Incomplete hostname 'Inception-Of-Things-P3' - must include both a name and a domain
-VBoxManage unattended install "${VM_NAME}" \
-    --full-user-name="${FULL_NAME}" \
-    --user="${USERNAME}" \
-    --password="${PASSWORD}" \
-    --locale=en_US \
-    --country=US \
-    --time-zone=UTC+1 \
-    --hostname="${VM_NAME}.local" \
-    --iso="${ISO_PATH}" \
-    --install-additions
+# # Copy cloud-init configuration file replacing "password" with the hashed password and
+# # copy it to a temporary directory along with meta-data from ./confs
+
+if [ -d /tmp/cloud-init ]; then
+    rm -rf /tmp/cloud-init
+fi
+mkdir /tmp/cloud-init
+# cp ./confs/user-data /tmp/cloud-init/user-data
+# cp ./confs/meta-data /tmp/cloud-init/meta-data
+# sed -i '' "s|passwordPlaceholder|$HASHED_PASSWORD|g" /tmp/cloud-init/user-data
+
+# !tmp
+cp ./confs/user-data /tmp/cloud-init/user-data
+cp ./confs/meta-data /tmp/cloud-init/meta-data
+hdiutil makehybrid -iso -joliet -default-volume-name cidata -o /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.iso /tmp/cloud-init
+# END !tmp
+
+# # Create FAT-32 file and convert it to vmdk
+# if [ -f /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg ]; then
+#     hdiutil detach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+#     rm -rf /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+# fi
+# hdiutil create -ov -size 100m -fs fat32 -volname cidata /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+# DISK_PATH=$(hdiutil attach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg | grep FDisk_partition_scheme | awk '{print $1}')
+# diskutil unmountDisk "${DISK_PATH}"
+# sleep 2
+# VBoxManage createmedium disk --filename /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.vmdk --format VMDK --variant RawDisk --property RawDrive="${DISK_PATH}"
+# diskutil mountDisk "${DISK_PATH}"
+# sleep 2
+
+# cp /tmp/cloud-init/user-data /Volumes/cidata/user-data
+# cp /tmp/cloud-init/meta-data /Volumes/cidata/meta-data
+
+# Make a FAT-32 formatted disk image and mount it
+# hdiutil create -ov -size 100m -fs fat32 -volname cidata /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+# hdiutil attach /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+
+# Create raw disk image
+# VBoxManage internalcommands createrawvmdk -filename /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.vmdk -rawdisk /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.dmg
+
+# Attach the Ubuntu to device 0 and cloud-init ISO to device 1
+VBoxManage storagectl "${VM_NAME}" --name "IDE Controller" --add ide
+VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "${ISO_PATH}"
+VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium /Users/sel-mars/goinfre/VirtualBox/${VM_NAME}/ubuntu-autoinstall.iso
+
+# Disable Floppy from booting and enable Hard Disk + CD/DVD
+VBoxManage modifyvm "${VM_NAME}" --boot1 disk --boot2 dvd --boot3 none --boot4 none
 
 # Start the VM
 VBoxManage startvm "${VM_NAME}" --type gui
