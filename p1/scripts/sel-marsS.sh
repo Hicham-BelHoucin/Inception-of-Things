09fe03d5be22038056586b2fcec088e75e7bf157
Soufiane El marsi <sel-mars@e2r3p6.1337.ma>
2025-01-04 14:54:30 +0100

    changed to jammy distibution + minor bug fixes

--- p1/scripts/sel-marsS.sh
+++ p1/scripts/sel-marsS.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip=192.168.56.110" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip=192.168.56.110 --flannel-iface=eth1" sh -
 cp /var/lib/rancher/k3s/server/node-token /vagrant/
